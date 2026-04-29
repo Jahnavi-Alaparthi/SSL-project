3e1ef7afecba5b05078296fe04fec8b58facc9c9
Jahnavi-Alaparthi <jahnavi.alaparthi99@gmail.com>
2026-04-29 12:28:16 +0530

    corrected the logic of leaderboard.sh

--- leaderboard.sh
+++ leaderboard.sh
@@ -1,35 +1,59 @@
-gamename=(Tictactoe Othello Connectfour)
+#!/bin/bash
 
 metric=$1
 
-# Determining which column to sort
 if [[ "$metric" == "win" ]]; then
-    col=3
+    sort_col=3
 elif [[ "$metric" == "loss" ]]; then
-    col=4
+    sort_col=4
 elif [[ "$metric" == "ratio" ]]; then
-    col=5
+    sort_col=5
+elif [[ "$metric" == "game" ]]; then
+    sort_col=1
 else
-    col=3  
-    # Default sorting win column
+    sort_col=3
 fi
 
-printf "%-12s %-12s %-5s %-5s %-6s\n" "Game" "User" "Win" "Loss" "Ratio"
-echo "---------------------------------------------------------"
-for game in "${gamename[@]}"; do
-    while IFS=$'\t' read username pwd; do
-        awk -F "," -v username="$username" -v game="$game" '
-        BEGIN {
-            loss=0; win=0
-        }
-       
-        $3 == game {
-            if ($1 == username) win++
-            if ($2 == username) loss++
-        }
-        END {
-            ratio = (loss == 0 ? win : win/loss)
-           printf "%-12s %-12s %-5d %-5d %-6.2f\n", game, username, win, loss, ratio
-        }' history.csv
-    done < users.tsv
-done | sort -k${col} -nr
+# printing the header
+printf "%-12s %-12s %-6s %-6s %-6s\n" "Game" "User" "Win" "Loss" "Ratio"
+echo "------------------------------------------------------------"
+
+awk -F "," '
+{
+    winner = $1
+    loser  = $2
+    game   = $4
+
+    #trim whitespace and carriage return characters
+    gsub(/\r/, "", game)
+    gsub(/^[ \t]+|[ \t]+$/, "", game)
+    gsub(/^[ \t]+|[ \t]+$/, "", winner)
+    gsub(/^[ \t]+|[ \t]+$/, "", loser)
+
+    win[game "|" winner]++
+    loss[game "|" loser]++
+
+    users[game "|" winner] = 1
+    users[game "|" loser] = 1
+}
+
+END {
+    for (k in users) {
+
+        split(k, a, "|")
+        game = a[1]
+        user = a[2]
+
+        w = win[game "|" user] + 0
+        l = loss[game "|" user] + 0
+
+        if (l == 0)
+            ratio = w
+        else
+            ratio = w / l
+
+        printf "%-12s %-12s %-6d %-6d %-6.2f\n",
+               game, user, w, l, ratio
+    }
+}
+' history.csv | sort -k1,1 -k${sort_col} -nr
